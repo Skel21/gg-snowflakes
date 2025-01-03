223f238f62e95e9fc409650b07e3242fd04d9041
Skel21 <bartlomiej.leks@gmail.com>
2025-01-03 21:52:23 +0100

    Add example Astro page

---
+++ astro/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://skel21.github.io',
+    base: '/gg-snowflakes',
+});
