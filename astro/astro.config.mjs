2815433ba78c6570ac5822b84007b013eac17481
Skel21 <bartlomiej.leks@gmail.com>
2025-11-25 10:00:02 +0100

    remove astro example

--- astro/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://skel21.github.io',
-    base: '/gg-snowflakes',
-});
